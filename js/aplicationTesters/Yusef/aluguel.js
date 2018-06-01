1d8a4c71ce018197f73f37007767e0b4b83484c6
yusefrich <yusef.rick@gmail.com>
2018-06-01 18:38:10 -0300

    pastas

--- js/aplicationTesters/Yusef/aluguel.js
+++ js/aplicationTesters/Yusef/aluguel.js
@@ -4,7 +4,6 @@ function Teste()
 {
 	
 }
-
 function RealizarAluguel()
 {
 
---
+++ js/aplicationTesters/Yusef/usuario.js
@@ -0,0 +1,38 @@
+var nome;
+var endereco;
+var cep;
+var telefone;
+var celular;
+var cpf;
+var nomeUsuario;
+var senha;
+var email;
+var dataNasc;
+var idCidade;
+var nomeCidade;
+var ufCidade;
+var siglaPais;
+var nomePais;
+
+class Usuario{
+
+}
+
+function alerta()
+{
+	alert('teste');
+}
+
+
+var Cliente = {
+	Cadastrar : function ()
+	{
+		//codigo para cadastro do cliente
+	}
+};
+var Funcionario = {
+	Cadastrar : function ()
+	{
+		//codigo para cadastro do cliente
+	}
+};
